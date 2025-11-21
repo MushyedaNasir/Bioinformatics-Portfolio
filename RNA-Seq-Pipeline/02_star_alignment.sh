b1eda5e31ad46ce1081217db9992b197570853ed
MushyedaNasir <mushyedanasir@gmail.com>
2025-11-21 16:22:44 +0500

    Update 02_star_alignment.sh

--- RNA-Seq-Pipeline/02_star_alignment.sh
+++ RNA-Seq-Pipeline/02_star_alignment.sh
@@ -3,13 +3,20 @@ GENOME_DIR="genome_index"   # diresctory name
 
 mkdir -p star_output
 
-for R1 in *_R1.fastq.gz
+for R1 in *_R1_001.fastq.gz
 do
-    SAMPLE=${R1%_R1.fastq.gz}
-    R2="${SAMPLE}_R2.fastq.gz"
+    # Remove the R1 part to extract sample name
+    SAMPLE=${R1%_R1_001.fastq.gz}
+
+    # Construct the R2 filename
+    R2="${SAMPLE}_R2_001.fastq.gz"
 
     echo "Processing sample: $SAMPLE"
+    echo "Forward: $R1"
+    echo "Reverse: $R2"
+    echo "-------------------------------------"
 
+    # Runing
     STAR \
       --runThreadN $THREADS \
       --genomeDir "$GENOME_DIR" \
